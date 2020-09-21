6fb6c16dc55bbd78040088bd21911f1b848336b6
Sebastian Glaser <hakt0r@users.noreply.github.com>
2020-09-21 11:01:47 +0200

    RegExp

---
+++ 07_AdvancedJS-RexExp.js
@@ -0,0 +1,24 @@
+
+/*
+    .match - true or false depending on the search
+    .split - return an array with the search as the "knife"
+
+    /^asd/ - will match the beginning of the string 
+    /asd$/ - will match the end of the string
+
+    /[a-z]/ - character classes allow you to specify
+    /[0-9]/   alternatives, one of the given
+    /\d/      needs to macht but not all of them
+
+    * . {}  - Quantifiers or multipiers help you
+              to define longer repetetive sections inside a
+              string. Usually use them in combination with a
+              character class.
+*/
+
+"asd".match( /a/  )      // true
+"asd".match( /^a/ )      // true
+"asd".match( /a$/ )      // false
+"asd".match( /^[as]/ )   // true
+"asd".match( /^[as]+/ )  // true
+"asd".match( /^[as]+$/ ) // false
